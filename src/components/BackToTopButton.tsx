77366515a78089122135d241b8e498628e457f8a
Daniel Mwamba <danmuamba81@gmail.com>
2025-09-05 11:37:51 +0100

    feat: added a tooltip to the "Back to Top" button and improved code formatting

--- src/components/BackToTopButton.tsx
+++ src/components/BackToTopButton.tsx
@@ -1,18 +1,15 @@
-
 import { useState, useEffect } from "react";
-import {ArrowUp} from "lucide-react"
-
+import { ArrowUp } from "lucide-react";
 
 export function BackToTopButton() {
-
-const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [showtooltip, setShowtooltip] = useState(false);
 
   useEffect(() => {
     const toggleVisibility = () => {
       if (window.scrollY > 200) {
         setVisible(true);
       } else {
-        
         setVisible(false);
       }
     };
@@ -33,10 +30,17 @@ const [visible, setVisible] = useState(false);
     <>
       {visible && (
         <button
+          onMouseEnter={() => setShowtooltip(true)}
+          onMouseLeave={() => setShowtooltip(false)}
           onClick={scrollToTop}
           className="fixed bottom-5 right-5 bg-primary text-white  w-10 h-10 sm:w-16 sm:h-16 flex items-center justify-center rounded-full shadow-lg hover:bg-pink-600 "
         >
-          < ArrowUp className="w-8 h-6" />
+          <ArrowUp className="w-8 h-6" />
+          {showtooltip && (
+            <div className="absolute bottom-10 right-10 bg-white text-secondary px-4 py-2 rounded-md shadow-lg">
+              Retour en haut
+            </div>
+          )}
         </button>
       )}
     </>
